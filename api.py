9a84e20040fd2df5d36fe694d9a71e4e6e114a35
miguelcpmarques <miguelcdpmarques@gmail.com>
2019-12-30 11:42:24 +0000

    fix: removed unwanted tensorflow exception

--- api.py
+++ api.py
@@ -15,9 +15,16 @@ def index():
 
 @app.route('/model', methods=['POST'])
 def model():
+    # Workaround to fix unwanted exception raised by Tensorflow
+    import keras.backend.tensorflow_backend as tb
+    tb._SYMBOLIC_SCOPE.value = True
+
+    # Saving file to folder
     file = request.files['photo']
     filename = secure_filename(file.filename)
     file.save(os.path.join('uploaded_photos', filename))
+
+    # Predicting label of saved file
     pipeline = PredictPipeline(json_model_path='model.json', model_weights_path='model_weights.h5',img_path='uploaded_photos/' + filename)
     prediction = pipeline.image_class_predict()
     return jsonify({'fruitName': prediction, 'predictionConfidence': 0.1})
